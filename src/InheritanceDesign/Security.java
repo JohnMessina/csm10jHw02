0e1b32772204e63f3f89580d1629b4c481dee49f
JohnMessina <j.w.messina24@gmail.com>
2015-11-10 17:38:15 -0800

    Update Security.java

--- src/InheritanceDesign/Security.java
+++ src/InheritanceDesign/Security.java
@@ -1,1 +1,35 @@
 
+package InheritanceDesign;
+
+/**
+ *
+ * @author mllroal
+ */
+abstract public class Security extends Asset
+{
+    public void setPrice(double pr)
+    {
+        price = pr;
+    }
+    
+    public void setQuantity(double q)
+    {
+        quantity = q;
+    }
+    
+    public double getPrice()
+    {
+        return price;
+    }
+    
+    public double getQuantity()
+    {
+        return quantity;
+    }
+    public double getAssetValue()
+    {
+        return quantity * price;
+    }
+    
+    private double price, quantity;
+}
