17834fdedbc3f36e0d4f2287e611412b9f49b43a
JohnMessina <j.w.messina24@gmail.com>
2015-11-10 17:38:34 -0800

    Create Stock.java

---
+++ src/InheritanceDesign/Stock.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package InheritanceDesign;
+
+/**
+ *
+ * @author mllroal
+ */
+public class Stock extends Security {
+    
+    Stock (String name, double price1, double quantity1)
+ {
+     setName(name);
+     setPrice(price1);
+     setQuantity(quantity1);
+ }
+    
+    
+    
+}
